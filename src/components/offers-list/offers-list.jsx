977bae898ca6a7a44f49d217079cf76d54daef0a
Mykhailo Nykoliuk <nykoliuk.m@ajax.systems>
2021-02-22 22:18:49 +0200

    Обратная сторона реальности

--- src/components/offers-list/offers-list.jsx
+++ src/components/offers-list/offers-list.jsx
@@ -1,15 +1,19 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import OfferCard from '../offer-card/offer-card';
 import offerTypes from '../../prop-types/offer-types';
 
 const OffersList = (props) => {
   const {offers} = props;
-  const [activeOffer, setActiveOffer] = useState();
+  const [activeOffer, setActiveOffer] = useState(null);
 
   const onHover = (id) => {
     setActiveOffer(id);
   };
+
+  useEffect(() => {
+    document.title = `Предложение ${activeOffer}`;
+  });
 
   return (
     <>
